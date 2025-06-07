561d8e99ad5876985667f6dd0ebcd9204bc6dd0e
Aniket Gaikwad <aniket199605@gmail.com>
2025-06-07 14:46:02 +0530

    Add files via upload

---
+++ YourTestClass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class YourTestClass {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2 + 2, 4);
+    }
+}
